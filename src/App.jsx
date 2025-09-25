e7f3c8c6dece14ec2df0ea3b2f61e58a7fb51241
Mignar99 <dariomelle18@gmail.com>
2025-09-25 13:13:36 +0000

    first modifications

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Dario Melle",
+  title: "PH.D Candidate in Theoretical Physics & Machine Learning and AI Specialist",
+  email: "dariomelle18@gmail.com",
+  gitHub: "Mignar99",
+  instagram: "dario.melle",
+  linkedIn: "dario-melle",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
